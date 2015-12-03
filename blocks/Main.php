7ce53652a02a6bb51eeee13d8abb630f331b1d3b
Ludovit Scholtz <ludko2@gmail.com>
2015-12-03 19:33:26 +0100

    dokončený formulár pre hlasovací preukaz odovzdany na zaklade splnomocnenia

--- blocks/Main.php
+++ blocks/Main.php
@@ -18,7 +18,7 @@ class Main extends \AsyncWeb\Frontend\Block{
 			while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {$i++;
 				if($i==1){
 					foreach($data as $k=>$v){
-						$n2k[Texts::clear($v)] = $k;	
+								$n2k[Texts::clear($v)] = $k;	
 					}
 				}else{
 					$db[$data[$n2k["ico"]]] = $data;
@@ -36,6 +36,8 @@ class Main extends \AsyncWeb\Frontend\Block{
 				$phpWord = $this->vytvorZiadostHlasovaciPreukazSplnomocnene($obec,$n2k);
 			}elseif(URLParser::v("how") == "postousr"){
 				$phpWord = $this->vytvorZiadostHlasovaniePostou($obec,$n2k);
+			}elseif(URLParser::v("how") == "postounonsr"){
+				$phpWord = $this->vytvorZiadostHlasovaniePostouNonSR($obec,$n2k);
 			}
 			if($phpWord){
 				$this->posliSubor($phpWord);		
@@ -64,13 +66,13 @@ class Main extends \AsyncWeb\Frontend\Block{
 		$ret.='<p>Ďalšie zdroje informácií:</p>
 		<ul>
 		<li><a target="_blank" href="https://platforma.slovensko.digital/t/registracia-na-volby-postou-zo-zahranicia-alebo-volicsky-preukaz/893/34">Diskusia o registrácii na hlasovanie</a></li>
-		<li><a target="_blank" href="http://www.minv.sk/?nr16-preukaz">Oficiálne informácie o hlasovaní hlasovacím preukazom</a></li>
+		<li><a target="_blank" href="http://www.minv.sk/?nr16-preukaz"><b>Oficiálne informácie o hlasovaní hlasovacím preukazom</b></a></li>
 		<li><a target="_blank" href="http://www.minv.sk/?nr16-preukaz&subor=230515">Oficiálna žiadosť o vydanie hlasovacieho preukazu (62,2 kB)</a></li>
 		<li><a target="_blank" href="http://www.minv.sk/?nr16-preukaz&subor=230517">Oficiálna žiadosť o vydanie hlasovacieho preukazu a splnomocnenie na jeho prevzatie (61,0 kB)</a></li>
 		<li><a target="_blank" href="http://www.minv.sk/?nr16-preukaz&subor=230518">Oficiálne splnomocnenie (58,6 kB)</a></li>
-		<li><a target="_blank" href="http://www.minv.sk/?nr16-posta2">Oficiálne informácie o hlasovaní hlasovaním poštou pre osoby s trvalým bydliskom na Slovensku</a></li>
+		<li><a target="_blank" href="http://www.minv.sk/?nr16-posta2"><b>Oficiálne informácie o hlasovaní hlasovaním poštou pre osoby s trvalým bydliskom na Slovensku</b></a></li>
 		<li><a target="_blank" href="http://www.minv.sk/?nr16-posta2&subor=220769">Oficiálna žiadosť o voľbu poštou pre voľby do Národnej rady Slovenskej republiky v roku 2016 (volič, ktorý má trvalý pobyt na území Slovenskej republiky a v čase volieb sa zdržiava mimo jej územia) (78,0 kB)</a></li>
-		<li><a target="_blank" href="http://www.minv.sk/?nr16-posta1">Oficiálne informácie o hlasovaní hlasovaním poštou pre osoby bez trvalého bydliska na Slovensku</a></li>
+		<li><a target="_blank" href="http://www.minv.sk/?nr16-posta1"><b>Oficiálne informácie o hlasovaní hlasovaním poštou pre osoby bez trvalého bydliska na Slovensku</b></a></li>
 		<li><a target="_blank" href="http://www.minv.sk/?nr16-posta1&subor=220768">Oficiálna žiadosť o voľbu poštou pre voľby do Národnej rady Slovenskej republiky v roku 2016 (volič, ktorý nemá trvalý pobyt na území Slovenskej republiky) (70,2 kB)</a></li>
 		<li><a target="_blank" href="http://www.minv.sk/?nr16-posta1&subor=230519">Čestné vyhlásenie (volič, ktorý nemá trvalý pobyt na území Slovenskej republiky) (36,2 kB)</a></li>		
 		</ul>
@@ -90,8 +92,9 @@ class Main extends \AsyncWeb\Frontend\Block{
         <div class="col-sm-10">
           <select id="how" name="how" class="form-control">
             <option value="preukaz">Chcem voliť osobne na Slovensku hlasovacím preukazom ktorý dostanem poštou do vlastných rúk</option>
-            <option value="preukazsplnomocnene">Chcem voliť osobne na Slovensku hlasovacím preukazom ktorý dostane splnomecnená osoba</option>
-            <option value="postousr">Chcem voliť poštou zo zahraničia</option>
+            <option value="preukazsplnomocnene">Chcem voliť osobne na Slovensku hlasovacím preukazom ktorý prevezme splnomecnená osoba</option>
+            <option value="postousr">Chcem voliť poštou zo zahraničia a mám trvalé bydlisko na Slovensku</option>
+            <option value="postounonsr">Chcem voliť poštou zo zahraničia a nemám trvalé bydlisko na Slovensku</option>
           </select>
         </div>
       </div>      <div class="form-group">
@@ -138,15 +141,15 @@ class Main extends \AsyncWeb\Frontend\Block{
 			$textrun = $section->addTextRun('pStyle');
 			$textrun->addTextBreak();
 			$textrun->addTextBreak();
-			$textrun->addText(htmlspecialchars("\t\t\t\t\t\t".$obec[$n2k["urad"]]),array('bold' => true));
+			$textrun->addText(htmlspecialchars("\t\t\t\t\t\t\t".$obec[$n2k["urad"]]),array('bold' => true));
 			$textrun->addTextBreak();
-			$textrun->addText(htmlspecialchars("\t\t\t\t\t\t".$obec[$n2k["ulica"]]." ".$obec[$n2k["cislo"]]));
+			$textrun->addText(htmlspecialchars("\t\t\t\t\t\t\t".$obec[$n2k["ulica"]]." ".$obec[$n2k["cislo"]]));
 			$textrun->addTextBreak();
-			$textrun->addText(htmlspecialchars("\t\t\t\t\t\t".$obec[$n2k["psc"]]." ".$obec[$n2k["posta"]]));
+			$textrun->addText(htmlspecialchars("\t\t\t\t\t\t\t".$obec[$n2k["psc"]]." ".$obec[$n2k["posta"]]));
 			$emails = explode(";",$obec[$n2k["email"]]);
 			foreach($emails as $email){
 				$textrun->addTextBreak();
-				$textrun->addText(htmlspecialchars("\t\t\t\t\t\t".$email));
+				$textrun->addText(htmlspecialchars("\t\t\t\t\t\t\t".$email));
 			}
 			$textrun->addTextBreak();
 			$textrun->addTextBreak();
@@ -195,7 +198,7 @@ class Main extends \AsyncWeb\Frontend\Block{
 			$textrun->addTextBreak();
 			$textrun->addText(htmlspecialchars("Meno:\t\t"));
 			$textrun->addText(htmlspecialchars("MENO"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
-			$textrun->addText(htmlspecialchars("\tPriezvisko:\t\t"));
+			$textrun->addText(htmlspecialchars("\t\tPriezvisko:\t\t"));
 			$textrun->addText(htmlspecialchars("PRIEZVISKO"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
 			$textrun->addTextBreak();
 			
@@ -218,44 +221,202 @@ class Main extends \AsyncWeb\Frontend\Block{
 			return $phpWord;
 	}
 	public function vytvorZiadostHlasovaciPreukazSplnomocnene($obec,$n2k){
-		
-	}
-	public function vytvorZiadostHlasovaniePostou($obec,$n2k){
 		$phpWord = new \PhpOffice\PhpWord\PhpWord();
-			/* Note: any element you append to a document must reside inside of a Section. */
+
 			$phpWord->addParagraphStyle('pStyle', array('spacing' => 5));
-			// Adding an empty Section to the document...
+			$phpWord->addParagraphStyle('pStyle2', array('spacing' => 5,'align' => 'center'));
+			$phpWord->addParagraphStyle('pStyler', array('spacing' => 5,'align' => 'right'));
+
 			$section = $phpWord->addSection();
-			// Adding Text element to the Section having font styled by default...
+
 			$textrun = $section->addTextRun('pStyle');
 			$textrun->addTextBreak();
 			$textrun->addTextBreak();
-			$textrun->addText(htmlspecialchars("\t\t\t\t\t\t".$obec[$n2k["urad"]]),array('bold' => true));
+			$textrun->addText(htmlspecialchars("\t\t\t\t\t\t\t".$obec[$n2k["urad"]]),array('bold' => true));
 			$textrun->addTextBreak();
-			$textrun->addText(htmlspecialchars("\t\t\t\t\t\t".$obec[$n2k["ulica"]]." ".$obec[$n2k["cislo"]]));
+			$textrun->addText(htmlspecialchars("\t\t\t\t\t\t\t".$obec[$n2k["ulica"]]." ".$obec[$n2k["cislo"]]));
 			$textrun->addTextBreak();
-			$textrun->addText(htmlspecialchars("\t\t\t\t\t\t".$obec[$n2k["psc"]]." ".$obec[$n2k["posta"]]));
-
+			$textrun->addText(htmlspecialchars("\t\t\t\t\t\t\t".$obec[$n2k["psc"]]." ".$obec[$n2k["posta"]]));
 			$emails = explode(";",$obec[$n2k["email"]]);
 			foreach($emails as $email){
 				$textrun->addTextBreak();
-				$textrun->addText(htmlspecialchars("\t\t\t\t\t\t".$email));
+				$textrun->addText(htmlspecialchars("\t\t\t\t\t\t\t".$email));
 			}
 			$textrun->addTextBreak();
 			$textrun->addTextBreak();
 			$textrun->addTextBreak();
 			$textrun->addTextBreak();
 			
-			$textrun->addText(htmlspecialchars("Vec: Žiadosť o zaradenie do zoznamu voličov mimo obce"),array('bold' => true));
+			$textrun->addText(htmlspecialchars("Vec: Žiadosť o vydanie hlasovacieho preukazu"),array('bold' => true));
 			$textrun->addTextBreak();			
+			$textrun->addTextBreak();				
+			$textrun->addText(htmlspecialchars("Meno:\t\t"));
+			$textrun->addText(htmlspecialchars("MENO"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
+
+			$textrun->addText(htmlspecialchars("\t\t\t"));
+			
+			$textrun->addText(htmlspecialchars("Priezvisko:\t\t"));
+			$textrun->addText(htmlspecialchars("PRIEZVISKO"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("R.č.:\t\t"));
+			$textrun->addText(htmlspecialchars("RODNÉ ČÍSLO"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
+			$textrun->addText(htmlspecialchars("\t\tŠtátna príslušnosť:\tSlovenská republika"));
+			
 			$textrun->addTextBreak();
 
-			$textrun->addText(htmlspecialchars("Žiadam o možnosť voľby v parlamentných voľbách v roku 2016 formou pošty."));
+			$textrun->addText(htmlspecialchars("Adresa trvalého pobytu:\t\t"));
+			$textrun->addText(htmlspecialchars("ADRESA TRVALÉHO BYDLISKA"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("(názov obce, názov ulice, ak sa obec člení na ulice, súpisné a orientačné číslo)"),array('size' => '6'));
+			
+			$textrun->addTextBreak();
+			$textrun->addTextBreak();			
+			
+			$textrun = $section->addTextRun('pStyle2');
+			$textrun->addText(htmlspecialchars("žiadam"),array('bold' => true,"align"=>"center"));
+			
+			$textrun = $section->addTextRun('pStyle');
 			$textrun->addTextBreak();
 			$textrun->addTextBreak();
-			$textrun->addText(htmlspecialchars("Žiadateľ:"));
+			$textrun->addText(htmlspecialchars("podľa § 46 zákona č. 180/2014 Z. z. o podmienkach výkonu volebného práva a o zmene a doplnení niektorých zákonov "));
+			$textrun->addText(htmlspecialchars("o vydanie hlasovacieho preukazu"),array('bold' => true));
+			$textrun->addText(htmlspecialchars(" pre voľby do Národnej rady Slovenskej republiky v roku 2016."));
+			
 			$textrun->addTextBreak();
-			$textrun->addTextBreak();				
+
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("Na prevzatie hlasovacieho preukazu podľa § 46 ods. 6 zákona "));
+			$textrun->addText(htmlspecialchars("splnomocňujem"),array('bold' => true));
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("Meno:\t\t"));
+			$textrun->addText(htmlspecialchars("MENO"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
+			$textrun->addText(htmlspecialchars("\t\tPriezvisko:\t\t"));
+			$textrun->addText(htmlspecialchars("PRIEZVISKO"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
+			$textrun->addTextBreak();
+			
+			$textrun->addText(htmlspecialchars("Číslo občianskeho preukazu:\t\t"));
+			$textrun->addText(htmlspecialchars("ČÍSLO OP SPLNOMOCNENEJ OSOBY"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
+			$textrun->addTextBreak();
+			$textrun->addTextBreak();
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("V "));
+			$textrun->addText(htmlspecialchars("MIESTO KDE SA NACHÁDZATE"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
+			$textrun->addText(htmlspecialchars(", dňa "));
+			$textrun->addText(htmlspecialchars("DNEŠNÝ DÁTUM"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
+			
+			$textrun->addTextBreak();
+			$textrun = $section->addTextRun('pStyler');
+			$textrun->addText(htmlspecialchars("MENO ALEBO PODPIS"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
+			
+			return $phpWord;
+	}
+	public function vytvorZiadostHlasovaniePostou($obec,$n2k){
+		$phpWord = new \PhpOffice\PhpWord\PhpWord();
+			/* Note: any element you append to a document must reside inside of a Section. */
+			$phpWord->addParagraphStyle('pStylel', array('spacing' => 5));
+			$phpWord->addParagraphStyle('pStylec', array('spacing' => 5,'align' => 'center'));
+			$phpWord->addParagraphStyle('pStyler', array('spacing' => 5,'align' => 'right'));
+			
+			$section = $phpWord->addSection();
+
+			$textrun = $section->addTextRun('pStyler');
+			$textrun->addText(htmlspecialchars("ŽIADOSŤ VYPLŇTE VEĽKÝMI PÍSMENAMI."),array('bold' => true));
+			$textrun->addTextBreak();
+			$textrun->addTextBreak();
+			$textrun = $section->addTextRun('pStylec');
+			$textrun->addText(htmlspecialchars("Žiadosť	o voľbu poštou"),array("size"=>"14"));
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("pre voľby do Národnej rady Slovenskej republiky v roku 2016"),array("size"=>"14"));
+			$textrun = $section->addTextRun('pStylel');
+			$textrun->addTextBreak();
+			$textrun->addTextBreak();
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("\t\t\t\t\t\t\t".$obec[$n2k["urad"]]),array('bold' => true));
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("\t\t\t\t\t\t\t".$obec[$n2k["ulica"]]." ".$obec[$n2k["cislo"]]));
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("\t\t\t\t\t\t\t".$obec[$n2k["psc"]]." ".$obec[$n2k["posta"]]));
+
+			$emails = explode(";",$obec[$n2k["email"]]);
+			foreach($emails as $email){
+				$textrun->addTextBreak();
+				$textrun->addText(htmlspecialchars("\t\t\t\t\t\t\t".$email));
+			}
+			$textrun->addTextBreak();
+			$textrun->addTextBreak();
+			$textrun->addTextBreak();
+			$textrun->addTextBreak();
+			
+
+			$textrun->addText(htmlspecialchars("Podľa § 60 ods. 1 zákona č. 180/2014 Z. z. o podmienkach výkonu volebného práva a o zmene a doplnení niektorých zákonov žiadam o voľbu poštou pre voľby do Národnej rady Slovenskej republiky v roku 2016."));
+			$textrun->addTextBreak();
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("Meno:\t\t\t\t"));
+			$textrun->addText(htmlspecialchars("MENO"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("Priezvisko:\t\t\t"));
+			$textrun->addText(htmlspecialchars("PRIEZVISKO"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("R.č.:\t\t\t\t"));
+			$textrun->addText(htmlspecialchars("RODNÉ ČÍSLO"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("Adresa:\t\t\t\t"));
+			$textrun->addText(htmlspecialchars("ADRESA TRVALÉHO BYDLISKA"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("Adresa kde zaslať hárky:\t"));
+			$textrun->addText(htmlspecialchars("ADRESA Z KADE CHCETE VOLIŤ"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
+			$textrun->addTextBreak();
+
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("S pozdravom,"),array('bold' => true));
+			$textrun->addTextBreak();
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("\t\t\t\t"));
+			$textrun->addText(htmlspecialchars("CELÉ MENO"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
+			return $phpWord;
+
+	}
+	public function vytvorZiadostHlasovaniePostouNonSR($obec,$n2k){
+		$phpWord = new \PhpOffice\PhpWord\PhpWord();
+			/* Note: any element you append to a document must reside inside of a Section. */
+			$phpWord->addParagraphStyle('pStylel', array('spacing' => 5));
+			$phpWord->addParagraphStyle('pStylec', array('spacing' => 5,'align' => 'center'));
+			$phpWord->addParagraphStyle('pStyler', array('spacing' => 5,'align' => 'right'));
+			
+			$section = $phpWord->addSection();
+
+			$textrun = $section->addTextRun('pStyler');
+			$textrun->addText(htmlspecialchars("ŽIADOSŤ VYPLŇTE VEĽKÝMI PÍSMENAMI."),array('bold' => true));
+			$textrun->addTextBreak();
+			$textrun->addTextBreak();
+			$textrun = $section->addTextRun('pStylec');
+			$textrun->addText(htmlspecialchars("Žiadosť	o voľbu poštou"),array("size"=>"14"));
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("pre voľby do Národnej rady Slovenskej republiky v roku 2016"),array("size"=>"14"));
+			$textrun = $section->addTextRun('pStylel');
+			$textrun->addTextBreak();
+			$textrun->addTextBreak();
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("\t\t\t\t\t\t\t".$obec[$n2k["urad"]]),array('bold' => true));
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("\t\t\t\t\t\t\t".$obec[$n2k["ulica"]]." ".$obec[$n2k["cislo"]]));
+			$textrun->addTextBreak();
+			$textrun->addText(htmlspecialchars("\t\t\t\t\t\t\t".$obec[$n2k["psc"]]." ".$obec[$n2k["posta"]]));
+
+			$emails = explode(";",$obec[$n2k["email"]]);
+			foreach($emails as $email){
+				$textrun->addTextBreak();
+				$textrun->addText(htmlspecialchars("\t\t\t\t\t\t\t".$email));
+			}
+			$textrun->addTextBreak();
+			$textrun->addTextBreak();
+			$textrun->addTextBreak();
+			$textrun->addTextBreak();
+			
+
+			$textrun->addText(htmlspecialchars("Podľa § 60 ods. 1 zákona č. 180/2014 Z. z. o podmienkach výkonu volebného práva a o zmene a doplnení niektorých zákonov žiadam o voľbu poštou pre voľby do Národnej rady Slovenskej republiky v roku 2016."));
+			$textrun->addTextBreak();
+			$textrun->addTextBreak();
 			$textrun->addText(htmlspecialchars("Meno:\t\t\t\t"));
 			$textrun->addText(htmlspecialchars("MENO"),array('bgColor' => \PhpOffice\PhpWord\Style\Font::FGCOLOR_YELLOW));
 			$textrun->addTextBreak();
